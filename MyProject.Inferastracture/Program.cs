9f2747ca2a9302f7d211b9b0a3e880b1a483d2d0
Ali <ali.256@yahoo.com>
2020-09-24 07:54:40 +0330

    init Project

---
+++ MyProject.Inferastracture/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyProject.Inferastracture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
